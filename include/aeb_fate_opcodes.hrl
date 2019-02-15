36f910aff4141bc1b3beb86b1a29bc1e6db00a36
Erik Stenman <erik@stenmans.org>
2019-02-15 21:44:36 +0100

    Add SWITCH opcodes.

--- include/aeb_fate_opcodes.hrl
+++ include/aeb_fate_opcodes.hrl
@@ -102,6 +102,11 @@
 -define('BLAKE2B'        , 16#67).
 -define('RETURNR'        , 16#68).
 -define('MAP_LOOKUPD'    , 16#69).
+-define('SWITCH_V2'      , 16#6a).
+-define('SWITCH_V3'      , 16#6b).
+-define('SWITCH_V4'      , 16#6c).
+-define('SWITCH_V5'      , 16#6d).
+-define('BITS_ALL_N'     , 16#6e).
 
 -define('FUNCTION'       , 16#fe).
 -define('EXTEND'         , 16#ff).

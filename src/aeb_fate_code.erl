5e9d34849f399d2624d480980d335a66d678777c
Erik Stenman <happi@happihacking.com>
2019-02-20 11:37:43 +0100

    Explicit export.

--- src/aeb_fate_code.erl
+++ src/aeb_fate_code.erl
@@ -1,9 +1,78 @@
 -module(aeb_fate_code).
 
-% TODO: rewrite to explicit exports.
--compile(export_all).
-
-%-export([]).
+-export([ return/0
+        , return/1
+        , call/1
+        , call_t/1
+        , call_r/2
+        , call_tr/2
+        , jump/1
+        , jumpif/2
+        , switch/3
+        , switch/4
+        , switch/5
+        , switch/6
+        , push/1
+        , inc/0
+        , inc/1
+        , dec/0
+        , dec/1
+        , add/3
+        , sub/3
+        , mul/3
+        , divide/3
+        , modulo/3
+        , pow/3
+        , lt/3
+        , gt/3
+        , elt/3
+        , egt/3
+        , eq/3
+        , neq/3
+        , and_op/3
+        , or_op/3
+        , not_op/2
+        , tuple/1
+        , element_op/4
+        , map_empty/1
+        , map_lookup/3
+        , map_lookup/4
+        , map_update/4
+        , map_member/3
+        , map_from_list/2
+        , nil/1
+        , is_nil/2
+        , cons/3
+        , hd/2
+        , tl/2
+        , length/2
+        , str_eq/3
+        , str_join/3
+        , int_to_str/2
+        , addr_to_str/2
+        , str_reverse/2
+        , int_to_addr/2
+        , variant_test/3
+        , variant_element/3
+        , variant/4
+        , bits_none/0
+        , bits_none/1
+        , bits_all/0
+        , bits_all/1
+        , bits_all_n/2
+        , bits_set/3
+        , bits_clear/3
+        , bits_test/3
+        , bits_sum/2
+        , bits_or/3
+        , bits_and/3
+        , bits_diff/3
+        , dup/0
+        , dup/1
+        , pop/0
+        , store/2
+        , nop/0
+        ]).
 
 -define(i(__X__), {immediate, __X__ }).
 

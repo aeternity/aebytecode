0528ee1229c10eb2432398b55b0f00a9284bca4e
Robert Virding <rvirding@gmail.com>
2019-03-20 23:34:23 +0100

    Fix (some) references to aeso_ types

--- include/aeb_heap.hrl
+++ include/aeb_heap.hrl
@@ -3,13 +3,13 @@
                val_t  :: aeso_sophia:type(),
                parent :: none | non_neg_integer(),
                size   = 0 :: non_neg_integer(),
-               data   :: #{aeso_heap:binary_value() => aeso_heap:binary_value() | tombstone}
+               data   :: #{aeb_heap:binary_value() => aeb_heap:binary_value() | tombstone}
                        | stored}).
 
 -record(maps, { maps    = #{} :: #{ non_neg_integer() => #pmap{} }
               , next_id = 0   :: non_neg_integer() }).
 
 -record(heap, { maps   :: #maps{},
-                offset :: aeso_heap:offset(),
+                offset :: aeb_heap:offset(),
                 heap   :: binary() | #{non_neg_integer() => non_neg_integer()} }).
 
